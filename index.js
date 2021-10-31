967eb153c0ebf28d6be2026f71479da1b60bf0e2
Mahmudtareq <tareqlpu5@gmail.com>
2021-10-31 21:54:35 +0600

    completed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port =process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-// connection 
+// connection setup
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l1qze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
